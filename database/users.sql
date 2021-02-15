09c2e781664731969788b862a581068028080e57
SalmaFarid998 <salmafareed998@gmail.com>
2021-02-15 19:14:53 +0200

    added table users to shop database

---
+++ database/users.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `shop`.`users` (
+ `UserID` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'To Identify User' ,
+ `Username` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'Username To Login' ,
+ `Password` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'Password To Login' ,
+ `Email` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+ `FullName` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+ `GroupID` INT(11) NOT NULL DEFAULT '0' COMMENT 'Identify User Group' ,
+ `TrustStatus` INT(11) NOT NULL DEFAULT '0' COMMENT 'Seller Rank' ,
+ `RegStatus` INT(11) NOT NULL DEFAULT '0' COMMENT 'User Approval' ,
+ PRIMARY KEY (`UserID`)
+ ) ENGINE = InnoDB;
